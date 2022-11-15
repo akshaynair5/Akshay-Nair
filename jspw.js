db1f8730aaa6737184e46427586834ce6fe8168d
Akshay Mathilakth Nair <108605741+akshaynair5@users.noreply.github.com>
2022-11-16 00:04:57 +0400

    Add files via upload

---
+++ jspw.js
@@ -0,0 +1,9 @@
+// let p = document.getElementById('resume');
+// let i = document.querySelector('res');
+
+// p.addEventListener('click', () =>{
+//     let path = i.getAttribute('src');
+//     let filename = "Akshay-nair-Resume";
+
+//     saveAs(path,filename);
+// })
